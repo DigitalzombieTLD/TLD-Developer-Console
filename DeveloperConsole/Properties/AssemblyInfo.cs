f2dd8d7899086f4bcd41e2b9225e190c1c12b0d1
Digitalzombie <blackdevil32@gmx.net>
2024-09-22 09:38:31 +0200

    TLDev compatibility

--- DeveloperConsole/Properties/AssemblyInfo.cs
+++ DeveloperConsole/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DeveloperConsole")]
 [assembly: AssemblyCopyright("Digitalzombie")]
-[assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyFileVersion("1.7.2")]
 [assembly: MelonInfo(typeof(DeveloperConsole.DeveloperConsole), "DeveloperConsole", "1.7.0", "Digitalzombie, FINDarkside, zeobviouslyfakeacc, ds5678", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
--- DeveloperConsole/SceneNameAutocompletePatch.cs
+++ DeveloperConsole/SceneNameAutocompletePatch.cs
@@ -6,7 +6,7 @@ using UnityEngine.ResourceManagement.ResourceLocations;
 using Il2CppCollection = Il2CppSystem.Collections.Generic;
 
 namespace DeveloperConsole {
-
+    /*
     [HarmonyPatch(typeof(BootUpdate), "Start")]
     internal static class SceneNameAutocompletePatch {
 
@@ -21,6 +21,36 @@ namespace DeveloperConsole {
             sceneParamaters.Sort();
 
             uConsoleAutoComplete.CreateCommandParameterSet("scene", sceneParamaters);
+        }
+    }*/
+
+    [HarmonyPatch(typeof(GameManager), nameof(GameManager.Awake))]
+    internal static class FixSceneAutoComplete
+    {
+
+        private static void Postfix()
+        {
+
+            foreach (uConsoleCommandParameterSet ccps in uConsoleAutoComplete.m_CommandParameterSets)
+            {
+                if (!ccps.m_Commands.Contains("scene"))
+                {
+                    return;
+                }
+
+                Il2CppCollection.List<IResourceLocation> scenes = AssetHelper.FindAllAssetsLocations<SceneSet>().Cast<Il2CppCollection.List<IResourceLocation>>();
+                Il2CppCollection.List<string> sceneParamaters = new Il2CppCollection.List<string>();
+
+                foreach (IResourceLocation sceneResource in scenes)
+                {
+                    if (sceneResource.PrimaryKey.ToLowerInvariant().StartsWith("mod") && !ccps.m_AllowedParameters.Contains(sceneResource.PrimaryKey))
+                    {
+                        ccps.m_AllowedParameters.Add(sceneResource.PrimaryKey);
+                        ccps.m_AllowedParameters.Add(sceneResource.PrimaryKey.ToLowerInvariant());
+                    }
+                }
+            }
+
         }
     }
 }
