888ea443a7a9a98038399c6c16027a5538cd6480
Digitalzombie <blackdevil32@gmx.net>
2026-04-01 15:47:37 +0200

    more stuff

--- DeveloperConsole/DeveloperConsoleSettings.cs
+++ DeveloperConsole/DeveloperConsoleSettings.cs
@@ -18,12 +18,52 @@ namespace DeveloperConsole
         [Slider(8, 22)]
         public int fontSize = 18;
 
+        [Section("Text color")]
+
+        [Name("Text - Red")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public byte textR = 0;
+
+        [Name("Text - Green")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public int textG = 0;
+
+        [Name("Text - Blue")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public int textB = 0;
+
+        [Section("Background color")]
+
+        [Name("Background - Red")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public int bgR = 185;
+
+        [Name("Background - Green")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public int bgG = 185;
+
+        [Name("Background - Blue")]
+        [Description(" ")]
+        [Slider(0, 255)]
+        public int bgB = 185;
+
+
         protected override void OnConfirm()
         {
             base.OnConfirm();
             uConsole.m_Instance.m_Activate = openConsoleButton;
             uConsole.m_Instance.m_LogFontSize = fontSize;
             uConsole.m_Instance.m_InputFieldFontSize = fontSize;
+            uConsole.m_Instance.m_InputFieldBackGroundColor = new Color32((byte)Settings.options.bgR, (byte)Settings.options.bgG, (byte)Settings.options.bgB, 255);
+            uConsole.m_Instance.m_InputFieldFontColor = new Color32((byte)Settings.options.textR, (byte)Settings.options.textG, (byte)Settings.options.textB, 255);
+            uConsole.m_Instance.m_LogBackGroundColor = new Color32((byte)Settings.options.bgR, (byte)Settings.options.bgG, (byte)Settings.options.bgB, 255);
+            uConsole.m_Instance.m_LogFontColor = new Color32((byte)Settings.options.textR, (byte)Settings.options.textG, (byte)Settings.options.textB, 255);
+
         }
     }
 
@@ -41,10 +81,10 @@ namespace DeveloperConsole
         {
             uConsole.m_Instance.m_Activate = Settings.options.openConsoleButton;
             uConsole.m_Instance.m_LogFontSize = Settings.options.fontSize;
-            //uConsole.m_Instance.m_InputFieldBackGroundColor = MaterialColors.Purple600;
-            //uConsole.m_Instance.m_InputFieldFontColor = Color.black;
-            //uConsole.m_Instance.m_LogBackGroundColor = MaterialColors.Pink400; 
-            //uConsole.m_Instance.m_LogFontColor = Color.black;
+            uConsole.m_Instance.m_InputFieldBackGroundColor = new Color32((byte)Settings.options.bgR, (byte)Settings.options.bgG, (byte)Settings.options.bgB, 255);
+            uConsole.m_Instance.m_InputFieldFontColor = new Color32((byte)Settings.options.textR, (byte)Settings.options.textG, (byte)Settings.options.textB, 255);
+            uConsole.m_Instance.m_LogBackGroundColor = new Color32((byte)Settings.options.bgR, (byte)Settings.options.bgG, (byte)Settings.options.bgB, 255);
+            uConsole.m_Instance.m_LogFontColor = new Color32((byte)Settings.options.textR, (byte)Settings.options.textG, (byte)Settings.options.textB, 255);
 
             uConsole.m_Instance.m_InputFieldFontSize = Settings.options.fontSize;
         }
--- DeveloperConsole/Properties/AssemblyInfo.cs
+++ DeveloperConsole/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DeveloperConsole")]
 [assembly: AssemblyCopyright("Digitalzombie")]
-[assembly: AssemblyVersion("1.8.5")]
-[assembly: AssemblyFileVersion("1.8.5")]
-[assembly: MelonInfo(typeof(DeveloperConsole.DeveloperConsole), "DeveloperConsole", "1.8.5", "Digitalzombie, FINDarkside, zeobviouslyfakeacc, ds5678", null)]
+[assembly: AssemblyVersion("1.8.6")]
+[assembly: AssemblyFileVersion("1.8.6")]
+[assembly: MelonInfo(typeof(DeveloperConsole.DeveloperConsole), "DeveloperConsole", "1.8.6", "Digitalzombie, FINDarkside, zeobviouslyfakeacc, ds5678", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
